c2b516515884d13eee2636cc71505e997f7c2158
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-09 16:55:54 +0200

    AUTOSAR Hotfixes

--- repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMemCheck.c
+++ repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMemCheck.c
@@ -1,24 +1,27 @@
 #include "Std_Types.hpp"
 
 #include "DcmMemCheck.hpp"
-#include "crc.hpp"
+#include "LibAutosar_Crc.hpp"
 #include "version.hpp"
 
 extern boolean DcmVerifyApplicationMemory(void){
-  tDescriptorBlock* pApplMetaData;
-  uint32 ulCrc32Calculated;
-  uint32 ulCrcRom;
-  boolean bRetVal = FALSE;
+   uint32 ulCrc32Calculated = LibAutosarCrc_u32Calculate(
+         (const uint8*)0x20100U
+      ,  0x57ae0U
+      ,  LibAutosarCrc_dValueInitial32
+      ,  TRUE
+   );
 
-  ulCrc32Calculated = Crc_CalculateCRC32((const uint8*)0x20100U, 0x57ae0U, CRC_INITIAL_VALUE32, TRUE);
+   tDescriptorBlock* pApplMetaData = VERSION_GetPointerToDescriptorBlock(cAPP);
 
-  pApplMetaData = VERSION_GetPointerToDescriptorBlock(cAPP);
-  ulCrcRom = *((uint32*)&(pApplMetaData->ucHash));
+   uint32 ulCrcRom = *((uint32*)&(pApplMetaData->ucHash));
 
-  if(ulCrcRom == ulCrc32Calculated)
-  {
-   bRetVal = TRUE;
-  }
-
-  return bRetVal;
+   
+   return (
+         ulCrcRom
+      == ulCrc32Calculated
+   )
+   ? TRUE
+   : FALSE
+   ;
 }
