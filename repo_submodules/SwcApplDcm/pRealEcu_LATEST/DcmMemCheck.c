c566dea9efd80d411a8b4f4187b2f103c69c8ee1
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-12 14:30:07 +0200

    AUTOSAR - Hotfixes - pRealEcu_cR_LATEST - SwcApplDcm

--- repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMemCheck.c
+++ repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMemCheck.c
@@ -1,27 +1,2 @@
 #include "Std_Types.hpp"
 
-#include "DcmMemCheck.hpp"
-#include "LibAutosar_Crc.hpp"
-#include "version.hpp"
-
-extern boolean DcmVerifyApplicationMemory(void){
-   uint32 ulCrc32Calculated = LibAutosarCrc_u32Calculate(
-         (const uint8*)0x20100U
-      ,  0x57ae0U
-      ,  LibAutosarCrc_dValueInitial32
-      ,  TRUE
-   );
-
-   tDescriptorBlock* pApplMetaData = VERSION_GetPointerToDescriptorBlock(cAPP);
-
-   uint32 ulCrcRom = *((uint32*)&(pApplMetaData->ucHash));
-
-   
-   return (
-         ulCrcRom
-      == ulCrc32Calculated
-   )
-   ? TRUE
-   : FALSE
-   ;
-}
--- repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMsgAuthenticationX.hpp
+++ repo_submodules/SwcApplDcm/pRealEcu_LATEST/DcmMsgAuthenticationX.hpp
@@ -10,6 +10,6 @@
 #define cSIZE_OF_HASH 16
 
 extern uint8 DcmCheckMessageAuthentication(uint8 ucSid, const uint8* aucMessage, uint16 ushLength);
-extern uint8 DcmAppendMessageAuthentication(uint8 ucSid, uint8* aucDataToAdd, uint32 ushLength, uint8* ucResponseMessage);
+extern uint8 DcmAppendMessageAuthentication(uint8 ucSid, uint8* alptru8DataToAdd, uint32 ushLength, uint8* ucResponseMessage);
 
 #endif //_DCM_MSG_AUTH_H
