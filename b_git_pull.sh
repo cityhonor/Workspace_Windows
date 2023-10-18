fc38b5ad3a3e036140abe88a57c082c9ba6e34d7
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-18 14:48:55 +0200

    Update

---
+++ b_git_pull.sh
@@ -0,0 +1,223 @@
+cd repo_submodules/CddBdrv
+git pull
+
+cd ../common
+git pull
+
+cd ../EcuabCanIf
+git pull
+
+cd ../EcuabCanTrcv
+git pull
+
+cd ../EcuabCryIf
+git pull
+
+cd ../EcuabEa
+git pull
+
+cd ../EcuabEthIf
+git pull
+
+cd ../EcuabFee
+git pull
+
+cd ../EcuabFrIf
+git pull
+
+cd ../EcuabFrTp
+git pull
+
+cd ../EcuabJ1939Tp
+git pull
+
+cd ../EcuabLinIf
+git pull
+
+cd ../EcuabLinTp
+git pull
+
+cd ../EcuabMemIf
+git pull
+
+cd ../EcuabWdgIf
+git pull
+
+cd ../LibAutosar
+git pull
+
+cd ../McalAdc
+git pull
+
+cd ../McalCan
+git pull
+
+cd ../McalCry
+git pull
+
+cd ../McalDio
+git pull
+
+cd ../McalEep
+git pull
+
+cd ../McalEth
+git pull
+
+cd ../McalFls
+git pull
+
+cd ../McalFr
+git pull
+
+cd ../McalGpt
+git pull
+
+cd ../McalIcu
+git pull
+
+cd ../McalLin
+git pull
+
+cd ../McalMcu
+git pull
+
+cd ../McalOcu
+git pull
+
+cd ../McalPort
+git pull
+
+cd ../McalPwm
+git pull
+
+cd ../McalSpi
+git pull
+
+cd ../McalWdg
+git pull
+
+cd ../ProjectARA
+git pull
+
+cd ../ProjectCfg
+git pull
+
+cd ../ProjectCompiler
+git pull
+
+cd ../ProjectConst
+git pull
+
+cd ../ReSimCmd
+git pull
+
+cd ../ReSimFm
+git pull
+
+cd ../SwcApplDcm
+git pull
+
+cd ../SwcApplDem
+git pull
+
+cd ../SwcApplEcuM
+git pull
+
+cd ../SwcApplFoc
+git pull
+
+cd ../SwcApplNvM
+git pull
+
+cd ../SwcApplStartUp
+git pull
+
+cd ../SwcApplTpms
+git pull
+
+cd ../SwcServiceBswM
+git pull
+
+cd ../SwcServiceCanNm
+git pull
+
+cd ../SwcServiceCanSm
+git pull
+
+cd ../SwcServiceCanTp
+git pull
+
+cd ../SwcServiceCom
+git pull
+
+cd ../SwcServiceComM
+git pull
+
+cd ../SwcServiceCsm
+git pull
+
+cd ../SwcServiceDcm
+git pull
+
+cd ../SwcServiceDem
+git pull
+
+cd ../SwcServiceDet
+git pull
+
+cd ../SwcServiceDlt
+git pull
+
+cd ../SwcServiceEcuM
+git pull
+
+cd ../SwcServiceFiM
+git pull
+
+cd ../SwcServiceFrNm
+git pull
+
+cd ../SwcServiceIoHwAb
+git pull
+
+cd ../SwcServiceIpduM
+git pull
+
+cd ../SwcServiceNm
+git pull
+
+cd ../SwcServiceNvM
+git pull
+
+cd ../SwcServiceOs
+git pull
+
+cd ../SwcServicePduR
+git pull
+
+cd ../SwcServiceSchM
+git pull
+
+cd ../SwcServiceSecOC
+git pull
+
+cd ../SwcServiceSokFm
+git pull
+
+cd ../SwcServiceStartUp
+git pull
+
+git pull
+
+cd ../SwcServiceStbM
+git pull
+
+cd ../SwcServiceVkms
+git pull
+
+cd ../SwcServiceWdgM
+git pull
+
+cd ../..
+git pull
