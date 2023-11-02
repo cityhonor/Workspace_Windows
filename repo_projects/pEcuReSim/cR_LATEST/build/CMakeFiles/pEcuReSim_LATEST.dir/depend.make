89215575edc1d26d51a44eb044585fcb8bd503e9
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-02 14:49:54 +0100

    AUTOSAR Migration Activities

--- repo_projects/pEcuReSim/cR_LATEST/build/CMakeFiles/pEcuReSim_LATEST.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pEcuReSim_LATEST.
-# This may be replaced when dependencies are built.
--- repo_projects/pEcuReSim/cR_LATEST/build/CMakeFiles/pEcuReSim_LATEST.dir/source/McalMcu/BrsStartup_c0.c.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/pEcuReSim_LATEST.dir/source/McalMcu/BrsStartup_c0.c.o: \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/McalMcu/BrsStartup_c0.c \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/common/Std_Types.hpp \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/ProjectCfg/CfgProject.hpp \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/common/Platform_Types.hpp \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/common/Compiler.hpp \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/ProjectCompiler/Compiler_Cfg.hpp \
- /cygdrive/c/_ECU2/ECU_Rivian_A1/Target/repo_projects/pEcuReSim/cR_LATEST/source/ProjectCompiler/CompilerCfg_SwcServiceOs.hpp
