7c096c6bb7e8d5c77a59e2223b5c8608a128abf4
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-02 15:45:11 +0100

    AUTOSAR Migration Activities

--- b_git_branch_main.sh
+++ b_git_branch_main.sh
@@ -1,497 +1,567 @@
-cd ./repo_projects/pEcuReSim
-echo "pEcuReSim"
-git pull
-git status
-echo ""
-
-cd ../pFOTA
+cd ./repo_projects/pFOTA
 echo "pFOTA"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../pIntegrationRdcXnf
 echo "pIntegrationRdcXnf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../pRealEcu
 echo "pRealEcu"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectWindows_Cygwin64_VirtualECUintelCOREi7
 echo "ProjectWindows_Cygwin64_VirtualECUintelCOREi7"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectWindows_Eclipse_VirtualECUintelCOREi7
 echo "ProjectWindows_Eclipse_VirtualECUintelCOREi7"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectWindows_uVisionV5p37_BldcFocSensorlessTLE9879QXA40
 echo "ProjectWindows_uVisionV5p37_BldcFocSensorlessTLE9879QXA40"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectWindows_VisualStudio_VirtualEcu
 echo "ProjectWindows_VisualStudio_VirtualEcu"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../../repo_submodules/CddBdrv
 echo "CddBdrv"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../common
 echo "common"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabCanIf
 echo "EcuabCanIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabCanTrcv
 echo "EcuabCanTrcv"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabCryIf
 echo "EcuabCryIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabEa
 echo "EcuabEa"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabEthIf
 echo "EcuabEthIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabFee
 echo "EcuabFee"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabFrIf
 echo "EcuabFrIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabFrTp
 echo "EcuabFrTp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabJ1939Tp
 echo "EcuabJ1939Tp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabLinIf
 echo "EcuabLinIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabLinTp
 echo "EcuabLinTp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabMemIf
 echo "EcuabMemIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../EcuabWdgIf
 echo "EcuabWdgIf"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../LibAutosar
 echo "LibAutosar"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalAdc
 echo "McalAdc"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalCan
 echo "McalCan"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalCry
 echo "McalCry"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalDio
 echo "McalDio"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalEep
 echo "McalEep"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalEth
 echo "McalEth"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalFls
 echo "McalFls"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalFr
 echo "McalFr"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalGpt
 echo "McalGpt"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalIcu
 echo "McalIcu"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalLin
 echo "McalLin"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalMcu
 echo "McalMcu"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalOcu
 echo "McalOcu"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalPort
 echo "McalPort"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalPwm
 echo "McalPwm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalSpi
 echo "McalSpi"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../McalWdg
 echo "McalWdg"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectARA
 echo "ProjectARA"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectCfg
 echo "ProjectCfg"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectCompiler
 echo "ProjectCompiler"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ProjectConst
 echo "ProjectConst"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ReSimCmd
 echo "ReSimCmd"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ReSimFm
 echo "ReSimFm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../ReSimVehicle
 echo "ReSimVehicle"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplDcm
 echo "SwcApplDcm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplDem
 echo "SwcApplDem"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplEcuM
 echo "SwcApplEcuM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplFoc
 echo "SwcApplFoc"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplNvM
 echo "SwcApplNvM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplStartUp
 echo "SwcApplStartUp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcApplTpms
 echo "SwcApplTpms"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceBswM
 echo "SwcServiceBswM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceCanNm
 echo "SwcServiceCanNm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceCanSm
 echo "SwcServiceCanSm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceCanTp
 echo "SwcServiceCanTp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceCom
 echo "SwcServiceCom"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceComM
 echo "SwcServiceComM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceCsm
 echo "SwcServiceCsm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceDcm
 echo "SwcServiceDcm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceDem
 echo "SwcServiceDem"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceDet
 echo "SwcServiceDet"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceDlt
 echo "SwcServiceDlt"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceEcuM
 echo "SwcServiceEcuM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceFiM
 echo "SwcServiceFiM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceFrNm
 echo "SwcServiceFrNm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceIoHwAb
 echo "SwcServiceIoHwAb"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceIpduM
 echo "SwcServiceIpduM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceNm
 echo "SwcServiceNm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceNvM
 echo "SwcServiceNvM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceOs
 echo "SwcServiceOs"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServicePduR
 echo "SwcServicePduR"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceSchM
 echo "SwcServiceSchM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceSecOC
 echo "SwcServiceSecOC"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceSokFm
 echo "SwcServiceSokFm"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceStartUp
 echo "SwcServiceStartUp"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceStbM
 echo "SwcServiceStbM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceVkms
 echo "SwcServiceVkms"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
 cd ../SwcServiceWdgM
 echo "SwcServiceWdgM"
-git pull
+git checkout main
+git branch -a
 git status
 echo ""
 
-cd ../..
-echo "Workspace"
-git pull
-git status
-echo ""
---
+++ repo_tools/GNU/Readme.txt
@@ -0,0 +1,1 @@
+To retrieve the source code, please contact application_support.flash-eu@lm.renesas.com
