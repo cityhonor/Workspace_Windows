d3f3b9079cd587944cecaedddd6653f93c111240
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-03-22 17:09:39 +0100

    Update!

---
+++ TimeMachine/202403220832AM_Confluence03_EcuConfiguration_05_ConfigureAndGenerateBsw_01_GenerateEcuc_PASSED/ecu_config/bsw/gen/ConfGen_version.h
@@ -0,0 +1,19 @@
+/* ********************************************************************************************************************
+ *
+ * COPYRIGHT RESERVED, 2013 ETAS GmbH. All rights reserved.
+ * The reproduction, distribution and utilization of this document as well as the communication of its contents to
+ * others without explicit authorization is prohibited. Offenders will be held liable for the payment of damages.
+ * All rights reserved in the event of the grant of a patent, utility model or design.
+ *
+ ******************************************************************************************************************* */
+
+#ifndef CONFGEN_VERSION_H
+#define CONFGEN_VERSION_H
+
+#define CONFGEN_VERSION "RTA-BSW ConfGen 12.2.0"
+#define CONFGEN_ISOLAR_AB "RTA-CAR 12.2.0"
+#define CONFGEN_GENERATED_ON "2024-03-22T08:27:36.1675294+01:00[Europe/Berlin]"
+
+// 
+
+#endif
